02dca71376417dfa6e052f3431827b8225599601
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2021-08-16 20:48:34 -0300

    Fazendo conforme foi pedido no trabalho!

--- Aula08_Exercicio02/Program.cs
+++ Aula08_Exercicio02/Program.cs
@@ -19,14 +19,18 @@ namespace Aula08_Exercicio02
             double n2 = double.Parse(Console.ReadLine());
 
             double som = n1 + n2;
+            Console.WriteLine("A soma é: " + som + "!");
             double sub = n1 - n2;
+            Console.WriteLine("A subtração é: " + sub + "!");
             double mult = n1 * n2;
+            Console.WriteLine("A multiplicação é: " + mult + "!");
             double divi = n1 / n2;
+            Console.WriteLine("A divisão é: " + divi + "!");
 
-            Console.WriteLine("A soma entre os números escolhidos é: " + som + "!"
-                + "\nA subtração entre os números escolhidos é: " + sub + "!"
-                + "\nA multiplicação entre os números escolhidos é: " + mult + "!"
-                + "\nA divisão entre os números escolhidos é: " + divi + "!");
+            //Console.WriteLine("A soma entre os números escolhidos é: " + som + "!"
+            //    + "\nA subtração entre os números escolhidos é: " + sub + "!"
+            //    + "\nA multiplicação entre os números escolhidos é: " + mult + "!"
+            //    + "\nA divisão entre os números escolhidos é: " + divi + "!");
 
             Console.ReadKey();
         }
